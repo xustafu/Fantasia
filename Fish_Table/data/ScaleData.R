dcf3fda814dee7c4f2812f6b80fa4e9d96d8b3e7
ivan-paz <ivnpaz@gmail.com>
2022-08-05 20:25:04 +0200

    fixing

--- Fish_Table/data/ScaleData.R
+++ Fish_Table/data/ScaleData.R
@@ -8,7 +8,10 @@ data <- data[1:256]
 
 data <-((data - min(data))*(32767-(-32767)))/(max(data)-min(data)) -32767
 data <- round(data)
+num <- paste(as.character(data), collapse=", ")
+num <- paste(as.character(num), sep="' '", collapse=", ")
 
+write(num)
 png("YourWaveSample.png", width = 20, height = 10, units = "cm", res = 300)
 plot(data, type="l", col="purple", xlab = "Time", ylab="Your data", lwd = 2)
 dev.off()
