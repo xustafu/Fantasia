f5e07f4a265f334abac7e1638fc2878f57cde4fe
xustafu <manu.retamero@gmail.com>
2025-10-15 19:00:12 +0200

    simpel sine + encoder + 7 segment example

--- Template_PlatformIO/src/main.cpp
+++ Template_PlatformIO/src/main.cpp
@@ -3,13 +3,21 @@
 #include <Audio.h>
 #include <Encoder.h>
 
-// Audio Declarations
-AudioSynthWaveform       waveform1;      //xy=684,318
-AudioOutputI2S           i2s1;           //xy=913,314
-AudioConnection          patchCord1(waveform1, 0, i2s1, 0);
-AudioConnection          patchCord2(waveform1, 0, i2s1, 1);
+// Audio TEST Declarations
+AudioSynthWaveformSine   sine1;          //xy=445.20001220703125,707.5999450683594
+AudioOutputI2S           i2s2;           //xy=837.1999969482422,711.6000213623047
+
+AudioConnection          patchCord1(sine1, 0, i2s2, 0);
+AudioConnection          patchCord2(sine1, 0, i2s2, 1);
+
 AudioControlSGTL5000     sgtl5000_1;     //xy=1125,313
-// GUItool: end automatically generated code
+// End automatically generated code
+
+// Segment display
+SegmentDisplay segmentDisplay(30, 32, 33, 28, 31, 26, 29, 9); // Pins connected to A, B, C, D, E, F, G, DP segments
+
+//Encoder init test
+Encoder myEnc(0, 1); // Pins connected to encoder A and B outputs
 
 // select the input pins for the potentiometers
 int PotPort1 = A0; 
@@ -49,8 +57,9 @@ void setup() {
     
   // open the serial port at 9600 bps:
   Serial.begin(9600); 
-  
-  // sets the digital pins as inputs and set pullups
+  Serial.println("Basic Encoder Test:");
+
+     // sets the digital pins as inputs and set pullups
   pinMode(Boton1, INPUT_PULLUP);   
   pinMode(Boton2, INPUT_PULLUP); 
   pinMode(Boton3, INPUT_PULLUP); 
@@ -71,7 +80,21 @@ void setup() {
   pinMode(9, OUTPUT); 
 }
 
+//Encoder test variable declarations
+long oldPosition  = -999;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Seven Segment Test code
+  segmentDisplay.testDisplay();
+  //Audio
+    sine1.frequency(440);
+    sine1.amplitude(0.4);
+  //Encoder test code
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+
+  } 
 }
 
