c60d25565e11ef0eae8597f02dbda36dfdf7a047
VanTa <fanta_e@hotmail.com>
2025-10-16 19:25:08 +0200

    alumbra day 3

---
+++ Tutorials/Drone_pio/src/main.cpp
@@ -0,0 +1,169 @@
+#include <Arduino.h>
+
+// use: https://github.com/dgduncan/SevenSegment
+// #include <SegmentDisplay.h>
+// https://github.com/newdigate/teensy-variable-playback
+// #include <TeensyVariablePlayback.h>
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include <Encoder.h>
+
+#define MAX_NUM_SOUND_BANKS 2
+#define MAX_NUM_SOUND_FILES 16
+#define BUTTON_UP 0
+#define BUTTON_DOWN 1
+
+// GUItool: begin automatically generated code
+AudioSynthWaveform waveform3;      // xy=158,499
+AudioSynthWaveform waveform4;      // xy=159,562
+AudioSynthWaveform waveform1;      // xy=160,373
+AudioSynthWaveform waveform2;      // xy=160,439
+AudioInputI2S i2s2;                // xy=163,292
+AudioMixer4 mixer1;                // xy=386,485
+AudioEffectBitcrusher bitcrusher1; // xy=519,487
+AudioFilterLadder filter_R;        // xy=659,519
+AudioFilterLadder filter_L;        // xy=661,455
+AudioOutputI2S i2s1;               // xy=863,478
+
+AudioConnection patchCord1(waveform3, 0, mixer1, 2);
+AudioConnection patchCord2(waveform4, 0, mixer1, 3);
+AudioConnection patchCord3(waveform1, 0, mixer1, 0);
+AudioConnection patchCord4(waveform2, 0, mixer1, 1);
+AudioConnection patchCord5(mixer1, bitcrusher1);
+AudioConnection patchCord6(bitcrusher1, 0, filter_L, 0);
+AudioConnection patchCord7(bitcrusher1, 0, filter_R, 0);
+AudioConnection patchCord8(filter_R, 0, i2s1, 1);
+AudioConnection patchCord9(filter_L, 0, i2s1, 0);
+
+AudioControlSGTL5000 sgtl5000_1; // xy=193,144
+// GUItool: end automatically generated code
+
+int button1_pin = 2;
+int button2_pin = 5;
+int button3_pin = 25;
+int button4_pin = 27;
+
+float freq1 = 440;
+float freq2 = 442;
+float freq3 = 446;
+float freq4 = 448;
+
+// Variables to store pot values
+int PotValue1 = 0;
+float Potmaped1 = 0;
+int PotValue2 = 0;
+float Potmaped2 = 0;
+int PotValue3 = 0;
+float Potmaped3 = 0;
+int PotValue4 = 0;
+float Potmaped4 = 0;
+
+int PotPort1 = A0; // select the input pins for the potentiometers
+int PotPort2 = A1;
+int PotPort3 = A2;
+int PotPort4 = A3;
+
+void setup() {
+
+  AudioMemory(120); // necesaria para que la libreria de audio funcione
+
+  // inicializamos el driver de audio
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(1);
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
+
+  // ponemos los botones en modo conectados HIGH por defecto
+  // en el esquematico vimos que al pulsar el boton se conectan a tierra (LOW)
+  pinMode(button1_pin, INPUT_PULLUP);
+  pinMode(button2_pin, INPUT_PULLUP);
+  pinMode(button3_pin, INPUT_PULLUP);
+  pinMode(button4_pin, INPUT_PULLUP);
+
+  analogReadResolution(12); // pasamos de una definicion de 1024 pasos a 4096
+
+  mixer1.gain(0, 1.0);
+  mixer1.gain(1, 1.0);
+  mixer1.gain(2, 1.0);
+  mixer1.gain(3, 1.0);
+
+  waveform1.amplitude(0.6);
+  waveform2.amplitude(0.6);
+  waveform3.amplitude(0.6);
+  waveform4.amplitude(0.6);
+
+  bitcrusher1.sampleRate(44100);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(button1_pin) == LOW)
+  {
+    waveform1.begin(0.6, freq1, WAVEFORM_SINE);
+    waveform2.begin(0.6, freq2, WAVEFORM_SINE);
+    waveform3.begin(0.6, freq3, WAVEFORM_SINE);
+    waveform4.begin(0.6, freq4, WAVEFORM_SINE);
+  }
+  if (digitalRead(button2_pin) == LOW)
+  {
+    waveform1.begin(WAVEFORM_SQUARE);
+    waveform2.begin(WAVEFORM_SQUARE);
+    waveform3.begin(WAVEFORM_SQUARE);
+    waveform4.begin(WAVEFORM_SQUARE);
+  }
+  if (digitalRead(button3_pin) == LOW)
+  {
+    waveform1.begin(WAVEFORM_TRIANGLE);
+    waveform2.begin(WAVEFORM_TRIANGLE);
+    waveform3.begin(WAVEFORM_TRIANGLE);
+    waveform4.begin(WAVEFORM_TRIANGLE);
+  }
+  if (digitalRead(button4_pin) == LOW)
+  {
+    waveform1.begin(WAVEFORM_SAWTOOTH);
+    waveform2.begin(WAVEFORM_SAWTOOTH);
+    waveform3.begin(WAVEFORM_SAWTOOTH);
+    waveform4.begin(WAVEFORM_SAWTOOTH);
+  }
+
+  
+
+  // read the value from the pots:
+  PotValue1 = analogRead(PotPort1);
+  PotValue2 = analogRead(PotPort2);
+  PotValue3 = analogRead(PotPort3);
+  PotValue4 = analogRead(PotPort4);
+
+  Potmaped1 = map(PotValue1, 0, 4095, 60, 12000);
+  filter_L.frequency(Potmaped1);
+  filter_R.frequency(Potmaped1);
+
+  // PotRounded2 = map(PotValue2, 0, 1023, 0, 1);
+
+  filter_L.resonance(0);
+  filter_R.resonance(0);
+
+  Potmaped3 = map(PotValue3, 0, 4095, 0, 4);
+
+  filter_L.inputDrive(Potmaped3); // range is 0 to 4.0
+  filter_R.inputDrive(Potmaped3);
+
+  Potmaped4 = map(PotValue4, 0, 4095, 16, 1);
+  bitcrusher1.bits(Potmaped4);
+
+  filter_L.passbandGain(0.5); // range is 0 to 0.5
+  filter_R.passbandGain(0.5);
+
+  freq1 = map(PotValue2, 0, 4095, 440, 512);
+  freq2 = map(PotValue2, 0, 4095, 440, 550);
+  freq3 = map(PotValue2, 0, 4095, 440, 600);
+  freq4 = map(PotValue2, 0, 4095, 440, 620);
+
+  waveform1.frequency(freq1);
+  waveform2.frequency(freq2);
+  waveform3.frequency(freq3);
+  waveform4.frequency(freq4);
+}
---
+++ Tutorials/Moog_pio/src/main.cpp
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// Audio declarations
+AudioControlSGTL5000 sgtl5000_1;
+
+// Code
+AudioInputI2S i2s2;         // xy=320.8833312988281,1692.88330078125
+AudioFilterLadder filter_L; // xy=521.88330078125,1644.5833740234375
+AudioFilterLadder filter_R; // xy=525.88330078125,1755.88330078125
+AudioOutputI2S i2s1;        // xy=734.88330078125,1688.5833740234375
+AudioConnection patchCord1(i2s2, 0, filter_L, 0);
+AudioConnection patchCord2(i2s2, 1, filter_R, 0);
+AudioConnection patchCord3(filter_L, 0, i2s1, 0);
+AudioConnection patchCord4(filter_R, 0, i2s1, 1);
+
+// SD CARD PINS
+#define SDCARD_CS_PIN 10
+#define SDCARD_MOSI_PIN 11
+#define SDCARD_SCK_PIN 13
+
+int PotPort1 = A0; // select the input pins for the potentiometers
+int PotPort2 = A1;
+int PotPort3 = A2;
+int PotPort4 = A3;
+
+int Boton1 = 2; // pushbutton connected to digital pinS
+int Boton2 = 5;
+int Boton3 = 25;
+int Boton4 = 27;
+int Gate1 = 3;
+int Gate2 = 6;
+int Encoderbut = 4;
+
+// Variables to store port values
+int PotValue1 = 0;
+float Potmaped1 = 0;
+int PotValue2 = 0;
+float PotRounded2 = 0;
+int PotValue3 = 0;
+float Potmaped3 = 0;
+int PotValue4 = 0;
+float Potmaped4 = 0;
+
+int BotonValue1 = 0;
+int BotonValue2 = 0;
+int BotonValue3 = 0;
+int BotonValue4 = 0;
+int GateValue1 = 0;
+int GateValue2 = 0;
+int EncoderbutValue = 0;
+
+void setup()
+{
+
+  // Audio setup
+  AudioMemory(8);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(1);
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
+  sgtl5000_1.micGain(36); // NEEDED?
+
+  Serial.begin(9600);            // open the serial port at 9600 bps:
+  pinMode(Boton1, INPUT_PULLUP); // sets the digital pin 2 as input
+  pinMode(Boton2, INPUT_PULLUP);
+  pinMode(Boton3, INPUT_PULLUP);
+  pinMode(Boton4, INPUT_PULLUP);
+  pinMode(Gate1, INPUT_PULLUP);
+  pinMode(Gate2, INPUT_PULLUP);
+  pinMode(Encoderbut, INPUT_PULLUP);
+}
+
+void loop()
+{
+
+  // read the value from the pots:
+  PotValue1 = analogRead(PotPort1);
+  PotValue2 = analogRead(PotPort2);
+  PotValue3 = analogRead(PotPort3);
+  PotValue4 = analogRead(PotPort4);
+
+  Potmaped1 = map(PotValue1, 0, 1023, 60, 12000);
+  filter_L.frequency(Potmaped1);
+  filter_R.frequency(Potmaped1);
+
+  PotRounded2 = map(PotValue2, 0, 1023, 0, 1);
+
+  filter_L.resonance(PotRounded2);
+  filter_R.resonance(PotRounded2);
+
+  Potmaped3 = map(PotValue3, 0, 1023, 0, 4);
+
+  filter_L.inputDrive(Potmaped3); // range is 0 to 4.0
+  filter_R.inputDrive(Potmaped3);
+
+  Potmaped4 = map(PotValue4, 0, 1023, 0.0, 0.5);
+
+  filter_L.passbandGain(Potmaped4); // range is 0 to 0.5
+  filter_R.passbandGain(Potmaped4);
+}
